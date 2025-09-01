5fe54dd2b79df1d9fa65761e094ee572973d6342
Krasorx <luiesp27@gmail.com>
2025-09-01 13:25:56 +0000

    fix params group notes redis

--- nihongo-app/app/notes/[id]/page.tsx
+++ nihongo-app/app/notes/[id]/page.tsx
@@ -1,6 +1,6 @@
 import NoteGroupPage from '../../components/noteGroup';
 import { redisApi } from '../../utils/api';
 
-export default function NoteGroup({ params }: { params: { hash: string } }) {
-  return <NoteGroupPage hashId={params.hash} endpoint="https://api.luisesp.cloud/api/redis/notes" />;
+export default function NoteGroup({ params }: { params: { id: string } }) {
+  return <NoteGroupPage hashId={params.id} endpoint="https://api.luisesp.cloud/api/redis/notes" />;
 }
